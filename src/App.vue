54ed54023e7e4dbfe03969c912a49ccbb925f2c9
drinkingtheink <jason.m.harrison@gmail.com>
2022-03-15 00:56:34 -0500

    adding deps

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,27 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { SVG } from '@svgdotjs/svg.js'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  mounted() {
+    console.log(`MOUNTED AND DRAWING ======+`);
+
+    // initialize SVG.js
+    var draw = SVG().addTo('body')
+
+    // draw pink square
+    draw.rect(100, 100).move(50, 50).fill('#f06')
+
+    setTimeout(() => {
+      console.log(`DOIN IT`)
+      draw.animate(2000, 1000, 'now').attr({ fill: '#f03' })
+    }, 2000);
+  },
 }
 </script>
 
